705a1cb2e9134d50a767a1dbf313ef9b0eada8a1
Daniela Gonzales <olabolacrayola@hotmail.com>
2018-03-19 10:15:26 -0500

    añade boostrap react

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// añadiendo Bootstrap
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
