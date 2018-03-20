08e3c5e1d10bfab949d6c60e71bb3a1c86015f5e
Betsy Vidal <ing.betsyvies@gmail.com>
2018-03-20 18:54:08 -0500

    añadiendo side-nav

--- src/components/app.js
+++ src/components/app.js
@@ -3,40 +3,72 @@ import React from 'react';
 const App = () => {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark primary-color">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="basicExampleNav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home
-              <span className="sr-only">(current)</span>
+    <div className="container-side-nav">
+      {/* <a href="#" data-activates="slide-out" className="btn btn-primary p-3 button-collapse"><i className="fa fa-bars"></i></a> */}
+      <div id="slide-out" className="side-nav fixed d-flex justify-content-center">
+        <ul className="custom-scrollbar padding">
+          <li className="container-list d-flex justify-content-center">
+            <div className="logo-wrapper waves-light">
+              <a href="#"><img id="logo-header" width="180" height="59" src="https://rextie-static-prod2.s3.amazonaws.com/img/rextie.d8f6b41d48f8.png" alt="Cambio de Moneda Online con Rextie"/></a>
+            </div>
+          </li>
+          <li className="container-list">
+            <div className="">
+              <h3 className="text-center">Daniela Gonzales</h3>
+              <p className="text-center">07163854</p>
+            </div>
+          </li>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-chevron-right container-icon"></span>
+              </span>
+              <span className="container-font">Comprar / Vender</span>
             </a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-user container-icon"></span>
+              </span>
+              <span className="container-font">Mis Datos</span>
+            </a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-license container-icon"></span>
+              </span>
+              <span className="container-font">Mis Cuentas</span>
+            </a>
           </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
-              </div>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-star container-icon"></span>
+              </span>
+              <span className="container-font">Mis Favoritos</span>
+            </a>
+          </li>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-chart-bars container-icon"></span>
+              </span>
+              <span className="container-font">Balance Historico</span>
+            </a>
+          </li>
+          <li className="container-list">
+            <a href="#" className="icons-sm d-flex align-items-center mt-4 mb-4">
+              <span className="mx-4">
+                <span className="lnr lnr-license container-icon"></span>
+              </span>
+              <span className="container-font">Cuentas Rextie</span>
+            </a>
           </li>
         </ul>
-        <form className="form-inline">
-          <div className="md-form mt-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-          </div>
-        </form>
+      </div>
     </div>
-  </nav>
   )
 }
 
