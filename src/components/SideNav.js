c345ebff5e6d6538a3eec0237ad0220e9e67cc64
Betsy Vidal <ing.betsyvies@gmail.com>
2018-03-21 10:26:18 -0500

    centrando el side-nav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -4,7 +4,8 @@ import UserData from './UserData';
 import ListNav from './ListNav'
 
 const SideNav = ({data}) => (
-  <div className="container-side-nav">
+  <div className="container-fluid">
+    <div className="row align-items-center heigth justify-content-center">
     {/* <a href="#" data-activates="slide-out" className="btn btn-primary p-3 button-collapse"><i className="fa fa-bars"></i></a> */}
     <div id="slide-out" className="side-nav d-flex justify-content-center">
       <ul className="custom-scrollbar padding">
@@ -12,6 +13,7 @@ const SideNav = ({data}) => (
         <UserData/>
         { data.map(list => <ListNav key = {list.name} {...list}/>)}
       </ul>
+    </div>
     </div>
   </div>
 )
