055014daaaff531ff4abad49d921c77fd1700034
Daniela Gonzales <olabolacrayola@hotmail.com>
2018-03-21 08:59:21 -0500

    añade transacción

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Transaction from './Transaction';
+import UserInformation from './UserInformation';
 
 const Main = () => (
   <div className="p-3 heigth-min">
-    <Transaction/>
+    {/* <Transaction/> */}
+    <UserInformation/>
   </div>
 );
 
