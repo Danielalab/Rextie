acce78b0093ffa379658614da91effea8342cd64
Daniela Gonzales <olabolacrayola@hotmail.com>
2018-03-21 09:56:08 -0500

    modifuca indentación

--- src/components/UserInformation.js
+++ src/components/UserInformation.js
@@ -1,48 +1,48 @@
 import React from 'react';
 
 const UserInformation = () => (
-    <div className="row justify-content-center">
-        <div className="card col-10 card-border">
-            <div className="card-body">
-                <div className="md-form">
-                    <input value="Daniela Gonzales" type="text" id="nameUser" className="form-control"/>
-                    <label htmlFor="nameUser">Nombre Completo</label>
-                </div>
-            </div>
+  <div className="row justify-content-center">
+    <div className="card col-10 card-border">
+      <div className="card-body">
+        <div className="md-form">
+          <input value="Daniela Gonzales" type="text" id="nameUser" className="form-control"/>
+          <label htmlFor="nameUser">Nombre Completo</label>
         </div>
-        <div className="card col-10 card-border">
-            <div className="card-body">
-                <div className="md-form">
-                    <input value="DNI" type="text" id="dni" className="form-control"/>
-                    <label htmlFor="dni">Tipo de Documento de Identidad</label>                    
-                </div>
-            </div>
-        </div>
-        <div className="card col-10 card-border">
-            <div className="card-body">
-                <div className="md-form">
-                    <input value="74352136" type="text" id="numberdni" className="form-control"/>
-                    <label htmlFor="numberdni">Número de Documento</label>                    
-                </div>
-            </div>
-        </div>
-        <div className="card col-10 card-border">
-            <div className="card-body">
-                <div className="md-form">
-                    <input value="Estudiante" type="text" id="work" className="form-control"/>
-                    <label htmlFor="work">Ocupación u Oficio</label>                    
-                </div>
-            </div>
-        </div>
-        <div className="card col-10 card-border">
-            <div className="card-body">
-                <div className="md-form">
-                    <input value="995526665" type="text" id="phone" className="form-control"/>
-                    <label htmlFor="phone">Celular</label>                    
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div className="card col-10 card-border">
+      <div className="card-body">
+        <div className="md-form">
+          <input value="DNI" type="text" id="dni" className="form-control"/>
+          <label htmlFor="dni">Tipo de Documento de Identidad</label>                    
+        </div>
+      </div>
+    </div>
+    <div className="card col-10 card-border">
+      <div className="card-body">
+        <div className="md-form">
+          <input value="74352136" type="text" id="numberdni" className="form-control"/>
+          <label htmlFor="numberdni">Número de Documento</label>                    
+        </div>
+      </div>
+    </div>
+    <div className="card col-10 card-border">
+      <div className="card-body">
+        <div className="md-form">
+          <input value="Estudiante" type="text" id="work" className="form-control"/>
+          <label htmlFor="work">Ocupación u Oficio</label>                    
+        </div>
+      </div>
+    </div>
+    <div className="card col-10 card-border">
+      <div className="card-body">
+        <div className="md-form">
+          <input value="995526665" type="text" id="phone" className="form-control"/>
+          <label htmlFor="phone">Celular</label>                    
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default UserInformation
