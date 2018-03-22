ce094fecfcf24c1ae9190048628583f7f698fd90
Betsy Vidal <ing.betsyvies@gmail.com>
2018-03-22 13:24:18 -0500

    añadiendo styles

--- src/components/ListNav.js
+++ src/components/ListNav.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ListNav = ({id, icon, name, navigateTo}) => (
   <li className="container-list mt-4 mb-4" onClick={() => navigateTo(id)}>
-    <a href="#" className="icons-sm d-flex align-items-center">
+    <div className="icons-sm d-flex align-items-center">
       <span className="mx-4">
         <span className={icon}></span>
       </span>
       <span className="container-font">{name}</span>
-    </a>
+    </div>
   </li>
 )
 
