a7acb66aa22961f6668458e44af7355197814f65
Daniela Gonzales <olabolacrayola@hotmail.com>
2018-03-30 21:26:32 -0500

    valida formulario de añadir cuenta

--- src/components/AddMyAccount.js
+++ src/components/AddMyAccount.js
@@ -1,5 +1,64 @@
 import React from 'react';
 
+//  inicia código de validación de formulario
+
+var regExpNumber = /^\d+$/
+
+let validateBank = false;
+let validateNumberAccount = false;
+let validateTypeAccount = false;
+let validateMoney = false;
+let disabledButton = true 
+
+function onChangeBank(bank) {
+  if (bank) {
+    validateBank = true;
+    allInputsValid();
+  } else {
+    validateBank = false;
+    disabledButton = true
+  }
+}
+
+function onChangeTypeAccount(account) {
+  if (account) {
+    validateTypeAccount = true;
+    allInputsValid();
+  } else {
+    validateTypeAccount = false;
+    disabledButton = true
+  }
+}
+
+function onChangeMoney(money) {
+  if (money) {
+    validateMoney = true;
+    allInputsValid();
+  } else {
+    validateMoney = false;
+    disabledButton = true;
+  }
+}
+
+function onChangeNumberAccount(number) {
+  if (regExpNumber.test(number)) {
+    validateNumberAccount = true;
+    allInputsValid()
+  } else {
+    validateNumberAccount = false;
+    disabledButton = true
+  }
+}
+
+
+function allInputsValid() {
+  if (validateBank && validateTypeAccount && validateMoney) {
+    disabledButton = false;
+  }
+}
+
+// inicia componente
+
 const AddMyAccount = ({navigateTo}) => (
   <div className="container-fluid">
     <div className="row justify-content-center">
@@ -10,55 +69,59 @@ const AddMyAccount = ({navigateTo}) => (
       <div className="form-row justify-content-center">
         <div className="form-group col-11 col-md-9">
           <label className="text-uppercase" for="inputdestination">Selecciona el Banco de tu Cuenta</label>
-          <select id="inputdestination" className="form-control">
-            <option>Selecciona tu Banco</option>
-            <option>BBVA</option>
-            <option>Scotiabank</option>
-            <option>BCP</option>
-            <option>Interbank</option>
-            <option>Citibank</option>
-            <option>Banco de la Nación</option>
-            <option>Banco de Comercio</option>
-            <option>Banco Financiero</option> 
-            <option>BanBif</option>
-            <option>Crediscotia Financiera</option>
-            <option>Mi Banco</option>           
-            <option>Agrobanco</option>           
-            <option>Banco GNB</option>           
-            <option>Banco Falabella</option>
-            <option>Banco Ripley</option>
-            <option>Banco Santander</option>                                  
-            <option>Banco Azteca</option>                                  
-            <option>ICBC</option>                                                                                                               
+          <select id="inputdestination" className="form-control" onChange={() => onChangeBank(event.target.value) }>
+            <option value="">Selecciona tu Banco</option>
+            <option value="BBVA">BBVA</option>
+            <option value="Scotiabank">Scotiabank</option>
+            <option value="BCP">BCP</option>
+            <option value="Interbank">Interbank</option>
+            <option value="Citibank">Citibank</option>
+            <option value="Banco de la Nación">Banco de la Nación</option>
+            <option value="Banco de Comercio">Banco de Comercio</option>
+            <option value="Banco Financiero">Banco Financiero</option> 
+            <option value="BanBif">BanBif</option>
+            <option value="Crediscotia Financiera">Crediscotia Financiera</option>
+            <option value="Mi Banco">Mi Banco</option>           
+            <option value="Agrobanco">Agrobanco</option>           
+            <option value="Banco GNB">Banco GNB</option>           
+            <option value="Banco Falabella">Banco Falabella</option>
+            <option value="Banco Ripley">Banco Ripley</option>
+            <option value="Banco Santander">Banco Santander</option>                                  
+            <option value="Banco Azteca">Banco Azteca</option>                                  
+            <option value="ICBC">ICBC</option>                                                                                                               
           </select>
         </div>
         <div className="form-group col-11 col-md-9">
+          <label className="text-uppercase" for="card-number">Número de Cuenta</label>
+          <input type="text" className="form-control" id="card-number" placeholder="Número de Cuenta" onChange={() => onChangeNumberAccount(event.target.value)}/>
+        </div>
+        <div className="form-group col-11 col-md-9">
           <label className="text-uppercase" for="inputOrigin">Selecciona el tipo de Cuenta</label>
-          <select id="inputOrigin" className="form-control">
-            <option>Elige el tipo de Cuenta</option>
-            <option>Cuenta de Ahorros</option>
-            <option>Cuenta Corriente</option>            
+          <select id="inputOrigin" className="form-control" onChange={()=> onChangeTypeAccount(event.target.value)}>
+            <option value="">Elige el tipo de Cuenta</option>
+            <option value="Ahorros">Cuenta de Ahorros</option>
+            <option value="Corriente">Cuenta Corriente</option>            
           </select>
         </div>
         <div className="form-group col-11 col-md-9">
           <label className="text-uppercase" for="inputOrigin">Selecciona el tipo de Cambio</label>
-          <select id="inputOrigin" className="form-control">
-            <option>Elige el tipo de Cambio</option>
-            <option>Soles</option>
-            <option>Dólares</option>            
+          <select id="inputOrigin" className="form-control" onChange={() => onChangeMoney(event.target.value)}>
+            <option value="">Elige el tipo de Cambio</option>
+            <option value="">Soles</option>
+            <option value="">Dólares</option>            
           </select>
         </div>
-        <div class="form-check mb-2 mr-sm-2">
-           <input class="form-check-input" type="checkbox" id="inlineFormCheckMD" />
-           <label class="form-check-label" for="inlineFormCheckMD">
+        <div className="form-check mb-2 mr-sm-2">
+          <input className="form-check-input" type="checkbox" id="inlineFormCheckMD" />
+          <label className="form-check-label" for="inlineFormCheckMD">
             Selecciona esta cuenta bancaria para tu próxima operación.
-        </label>
-    </div>
+          </label>
+        </div>
       </div>
     </form>
     <div className="row justify-content-center">
       <a className="btn btn-back" onClick={() => navigateTo('myAccounts')}>Cancelar</a>
-      <a className="btn btn-success" onClick={() => navigateTo('myAccounts')}>Añadir</a> 
+      <a className="btn btn-success" disabled={disabledButton ? "disabled" : false} onClick={() => navigateTo('myAccounts')}>Añadir</a> 
     </div>     
     </div>
     </div>
