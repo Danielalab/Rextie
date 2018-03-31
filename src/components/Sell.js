42fc4ea203673db718e6fbf3605dd80d7d8d8976
Betsy Vidal <ing.betsyvies@gmail.com>
2018-03-30 22:58:00 -0500

    añadiendo valor de compra y venta del dolar

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -2,36 +2,45 @@ import React from 'react';
 
 const Sell = ({navigateTo}) => (
   <div>
-  <br/>
-  <form>
-    <div className="row align-items-end">
-      <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
-        <label className="font-weight-bold" for="exampleForm2">Vender</label>
-        <input type="text" className="form-control disabled disabled-input " placeholder="USD" id="exampleForm2"/>
+    <br/>
+    <h4 className="title-cuenta-rextie">1 USD - 3.1900 PEN</h4>
+    <br/>
+    <form>
+      <div className="row align-items-end">
+        <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
+          <label className="font-weight-bold" for="exampleForm2">Vender</label>
+          <input type="text" className="form-control disabled disabled-input " placeholder="USD" id="exampleForm2"/>
+        </div>
+        <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
+          <label className="font-weight-bold" for="exampleForm1">Cantidad</label>
+          <input type="text" className="form-control" placeholder="0" id="exampleForm1"/>
+        </div>
+        <div className="col-12 col-sm-1 text-sm-center text-md-left my-sm-3">
+          <p className="text-center font-operator align-text-bottom m-0 color-3">X</p>
+        </div>
+        <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
+          <label className="font-weight-bold" for="exampleForm2">Precio por USD</label>
+          <input type="text" className="form-control disabled disabled-input " placeholder="0" id="exampleForm2"/>
+        </div>
+        <div className="col-12 col-sm-1 text-sm-center text-md-left my-sm-3">
+          <p className="text-center font-operator align-text-bottom m-0 color-3">=</p>
+        </div>
+        <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
+          <label className="font-weight-bold" for="exampleForm2">PEN a recibir</label>
+          <input type="text" className="form-control" placeholder="0" id="exampleForm2"/>
+        </div>
+        <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
+          <button type="button" className="btn btn-success"onClick={()=> navigateTo('confirmation')} >Success</button>
+        </div>
       </div>
-      <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
-        <label className="font-weight-bold" for="exampleForm1">Cantidad</label>
-        <input type="text" className="form-control" placeholder="0" id="exampleForm1"/>
-      </div>
-      <div className="col-12 col-sm-1 text-sm-center text-md-left my-sm-3">
-        <p className="text-center font-operator align-text-bottom m-0 color-3">X</p>
-      </div>
-      <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
-        <label className="font-weight-bold" for="exampleForm2">Precio por USD</label>
-        <input type="text" className="form-control disabled disabled-input " placeholder="0" id="exampleForm2"/>
-      </div>
-      <div className="col-12 col-sm-1 text-sm-center text-md-left my-sm-3">
-        <p className="text-center font-operator align-text-bottom m-0 color-3">=</p>
-      </div>
-      <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
-        <label className="font-weight-bold" for="exampleForm2">PEN a recibir</label>
-        <input type="text" className="form-control" placeholder="0" id="exampleForm2"/>
-      </div>
-      <div className="col-12 col-sm text-sm-center text-md-left my-sm-3">
-        <button type="button" className="btn btn-success"onClick={()=> navigateTo('confirmation')} >Success</button>
-      </div>
+    </form>
+    <br/>
+    <div className="d-flex justify-content-center">
+      <h5>Ahorra S/ 14.21 respecto al banco</h5>
     </div>
-  </form>
+    <div className="d-flex justify-content-center">
+      <p>Tipo de cambio vigente hasta 22:14</p>
+    </div>
   </div>
 )
 export default Sell;
