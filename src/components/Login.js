392d1e7ff2e8f54e99cc413364625322d91e78f5
Betsy Vidal <ing.betsyvies@gmail.com>
2018-04-01 21:44:35 -0500

    aceptando estilos

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from 'firebase';
 import { firebaseApp } from '../firebase';
+import Logo from './Logo';
 
 function saveData(user) {
   var users = {
@@ -14,22 +15,44 @@ function saveData(user) {
 
 const Login = ({loginUser, dataFirebaseUser}) => (
   <div className="container-fluid">
-    <button onClick={() => {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
-        .then(result => {
-          // The signed-in user info.
-          var user = result.user;
-          // Llamamos a la funcion
-          saveData(result.user);
-          loginUser('homePage');
-        })
-        .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+    <div className="row justify-content-center align-items-center heigth" >
+      <div className="col-11 col-md-5">
+        <div className="card mx-xl-5 pb-3">
+          <div className="card-body">
+            <Logo />
+            <form>
+              <p className="h5 text-center mb-4">Ingresa tu correo electrónico y contraseña para ingresar a Rextie.com.</p> 
+              <label for="defaultFormLoginEmailEx" className="grey-text">Tu email</label>
+              <input type="email" id="defaultFormLoginEmailEx" className="form-control"/>
+              <br/>
+              <label for="defaultFormLoginPasswordEx" className="grey-text">Tu contraseña</label>
+              <input type="password" id="defaultFormLoginPasswordEx" className="form-control"/>
+              <div className="text-center mt-4">
+                <button className="btn btn-neutro-1" type="submit" disabled>Iniciar sesión</button>
+                <button className="btn btn-neutro-2" onClick={(event) => {
+                  event.preventDefault();
+                  
+                  var provider = new firebase.auth.GoogleAuthProvider();
+                  firebase.auth().signInWithPopup(provider)
+                    .then(result => {
+                    // The signed-in user info.
+                    var user = result.user;
+                    // Llamamos a la funcion
+                    saveData(result.user);
+                    loginUser('homePage');
+                  })
+                  .catch(error => console.log(`Error ${error.code}: ${error.message}`))
 
-        firebaseApp.auth().onAuthStateChanged(user => dataFirebaseUser(user))
-      }}>
-      Inicia Sesión
-    </button>
+                  firebaseApp.auth().onAuthStateChanged(user => dataFirebaseUser(user))
+                  }}>
+                  <i className="fa fa-google-plus mr-2 font-2x"></i>Google 
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
